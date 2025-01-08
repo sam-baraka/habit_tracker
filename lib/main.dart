22e46b16481a957952abec972a8c1a29362ef030
Samuel Baraka <sababuvercetti@gmail.com>
2025-01-08 13:13:07 +0300

    Refactor main.dart to integrate HabitBloc and HabitRepository
    - Added HabitBloc and HabitRepository to the main application for enhanced habit management.
    - Initialized Hive boxes for storing habits and user data.
    - Updated HabitTracker widget to support multiple BLoCs using MultiBlocProvider.
    - Improved repository initialization and dependency injection for better code organization.

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,64 @@ import 'package:solutech_interview/firebase_options.dart';
 import 'package:solutech_interview/routes/router.dart';
 import 'package:solutech_interview/data/repositories/auth_repository.dart';
 import 'package:solutech_interview/presentation/blocs/auth/auth_bloc.dart';
+import 'package:solutech_interview/presentation/blocs/habit/habit_bloc.dart';
+import 'package:solutech_interview/data/repositories/habit_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  
+
   // Initialize Hive
   await Hive.initFlutter();
   
+  // Open Hive boxes
+  await Hive.openBox<Map>('habits');
+  await Hive.openBox<Map>('user_data');
+
+  // Initialize repositories
   final authRepository = AuthRepository();
-  
-  runApp(HabitTracker(authRepository: authRepository));
+  final habitRepository = HabitRepository();
+
+  // Initialize repositories
+  await HabitRepository.init();
+
+  runApp(HabitTracker(
+    authRepository: authRepository,
+    habitRepository: habitRepository,
+  ));
 }
 
 class HabitTracker extends StatelessWidget {
   final AuthRepository authRepository;
-  
+  final HabitRepository habitRepository;
+
   const HabitTracker({
     super.key,
     required this.authRepository,
+    required this.habitRepository,
   });
 
   @override
   Widget build(BuildContext context) {
     final _appRouter = AppRouter();
-    
-    return BlocProvider(
-      create: (context) => AuthBloc(
-        authRepository: authRepository,
-      ),
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc(
+            authRepository: authRepository,
+          ),
+        ),
+        BlocProvider(
+          create: (context) => HabitBloc(
+            habitRepository: habitRepository,
+          ),
+        ),
+      ],
       child: MaterialApp.router(
         title: 'Habit Tracker',
         theme: AppTheme.lightTheme,
